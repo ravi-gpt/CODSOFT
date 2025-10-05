b5c74c4c13a8c31017440afdaacc94afdc48f44e
Ravi Gupta <ravigupta924790@gmail.com>
2025-10-05 23:11:10 +0530

    Renamed NumberGuess.java to Task1.java

--- Task1.java
+++ Task1.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGuess {
+public class Task1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         Random rand=new Random();
